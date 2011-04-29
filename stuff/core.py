2fed24ca803050f41f761a52d9052be239481b99
lcr <none@none>
2011-04-29 15:33:21 -0600

    - clean

--- stuff/core.py
+++ stuff/core.py
@@ -1,65 +1,52 @@
-'''appspace utilities'''
+'''stuff'''
 
-from functools import wraps
-from collections import OrderedDict
+from collections import defaultdict
+try:
+    from collections import OrderedDict
+except ImportError:
+    from stuff.compat import OrderedDict
 
-def lru_cache(maxsize=100):
-    '''Least-recently-used cache decorator.
-
-    From Raymond Hettinger
-
-    Arguments to the cached function must be hashable.
-
-    @param maxsize: maximum number of results in LRU cache
-    '''
-    def wrapped(func):
-        # order: least recent to most recent
-        cache = OrderedDict()
-        @wraps(func)
-        def wrapper(*args, **kw):
-            key = args
-            if kw: key += tuple(sorted(kw.items()))
-            try:
-                result = cache.pop(key)
-            except KeyError:
-                result = func(*args, **kw)
-                # purge least recently used cache entry
-                if len(cache) >= maxsize: cache.popitem(0)
-            # record recent use of this key
-            cache[key] = result
-            return result
-        return wrapper
-    return wrapped
+from stuff.util import lru_cache
 
 
-class lazy(object):
+class switchdict(dict):
 
-    '''Lazily assign attributes on an instance upon first use.'''
-
-    def __init__(self, method):
-        self.method = method
-        try:
-            self.__doc__ = method.__doc__
-            self.__module__ = method.__module__
-            self.__name__ = method.__name__
-        except:
-            pass
-
-    def __get__(self, instance, cls=None):
-        if instance is None: return self
-        value = self.method(instance)
-        setattr(instance, self.method.__name__, value)
-        return value
+    def __new__(cls, **kw):
+        factory = kw.pop('factory', False)
+        if factory: return defaultdict(factory)
+        order = kw.pop('order', False)
+        if order: return OrderedDict
+        return dict
 
 
-class _commonstuff(dict):
+class stuffdict(switchdict):
+
+    def __new__(cls, *arg, **kw):
+        if isinstance(arg[0], dict):
+            if len(arg) > 1: raise TypeError('Invalid number of arguments')
+            kw.update(arg[0])
+            return super(cls, stuffdict).__new__(**kw).__init__(
+                (k, v) for k, v in kw.iteritems()
+            )
+        elif isinstance(arg, (list, tuple)):
+            return super(cls, stuffdict).__new__(**kw).__init__(
+                (k, v) for k, v in arg
+            )
+        raise TypeError('Invalid type for stuff')
+
+
+class _commonstuff(switchdict):
 
     def __new__(cls, *arg, **kw):
         if isinstance(arg[0], dict):
             kw.update(arg[0])
-            return cls((k, cls.__new__(v)) for k, v in kw.iteritems())
+            return super(cls, stuffdict).__new__(**kw).__init__(
+                (k, cls.__init__(v)) for k, v in kw.iteritems()
+            )
         elif isinstance(arg, (list, tuple)):
-            return type(arg)(cls.__new__(v) for v in arg)
+            return super(cls, stuffdict).__new__(**kw).__init__(
+                (k, cls.__init__(v)) for k, v in arg
+            )
         raise TypeError('Invalid type for stuff')
 
     def __contains__(self, k):
@@ -75,7 +62,7 @@ class _commonstuff(dict):
 
     def __repr__(self):
         args = ', '.join(
-            list('%s=%r' % (key, self[key]) for key in sorted(self.iterkeys()))
+            list('%s=%r' % (k, self[k]) for k in sorted(self.iterkeys()))
         )
         return '%s(%s)' % (self.__class__.__name__, args)
 
