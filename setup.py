35ff485ee2a7dd5db166e4e471daca88ef285d91
lcr <kwarterthieves@yahoo.com>
2011-12-27 20:53:21 -0700

    - update version, keywords

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info[0] == 2 and sys.version_info[1] < 7:
 
 setup(
     name='stuf',
-    version='0.7.2',
+    version='0.8.0',
     description='''stuf has attributes''',
     long_description=open(os.path.join(os.getcwd(), 'README.rst'), 'r').read(),
     author='L. C. Rees',
@@ -23,7 +23,7 @@ setup(
     packages=['stuf'],
     test_suite='stuf.test',
     zip_safe=False,
-    keywords='dict attribute collection mapping dot notation access',
+    keywords='dict attribute collection mapping dot notation access bunch',
     install_requires=install_requires,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
