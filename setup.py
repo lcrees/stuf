1b78e0d3546850e6054d35f3a27554fe7b48ccbe
lcr <kwarterthieves@yahoo.com>
2012-02-20 02:14:18 -0700

    - update
    --HG--
    branch : pu

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info[0] == 2 and sys.version_info[1] < 7:
 
 setup(
     name='stuf',
-    version='0.8.3',
+    version='0.8.4',
     description='''stuf has attributes''',
     long_description=open(os.path.join(os.getcwd(), 'README.rst'), 'r').read(),
     author='L. C. Rees',
