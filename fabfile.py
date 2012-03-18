88a5611ab1a30492b9310fa53f23f4e0a8bbadff
lcr <kwarterthieves@yahoo.com>
2012-03-17 23:38:40 -0600

    - setup for testing
    --HG--
    branch : pu

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+'''stuf fabfile'''
+
+from fabric.api import prompt, local, settings, env
+
+
+def tox():
+    '''test stuf'''
+    local('tox')
+
+
+def release():
+    '''release stuf'''
+    local('hg update next')
+    local('hg merge pu')
+    local('hg update maint')
+    local('hg merge default')
+    local('hg update default')
+    local('hg merge next')
+    local('hg update pu')
+    local('hg merge default')
+    prompt('Enter tag', 'tag')
+    with settings(warn_only=True):
+        local('hg tag "%(tag)s"' % env)
+    local('hg push ssh://hg@bitbucket.org/lcrees/stuf')
+    local('hg push git+ssh://git@github.com:kwarterthieves/stuf.git')
+    local('python setup register sdist --format=bztar,gztar,zip, upload')
