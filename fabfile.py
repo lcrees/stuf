c36ee5ee7c51d1014801c2cad64ab38d1429cc52
lcr <kwarterthieves@yahoo.com>
2012-03-18 00:26:17 -0600

    - update fab
    --HG--
    branch : pu

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,26 @@
 from fabric.api import prompt, local, settings, env
 
 
+def _test(val):
+    truth = val in ['py26', 'py27', 'py32']
+    if truth is False:
+        raise KeyError(val)
+    return val
+
+
 def tox():
     '''test stuf'''
     local('tox')
+
+
+def tox_recreate():
+    '''recreate stuf test env'''
+    prompt(
+        'Enter testenv: [py26, py27, py32]',
+        'testenv',
+        validate=_test,
+    )
+    local('tox --recreate -e %(testenv)s' % env)
 
 
 def release():
