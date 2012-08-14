1419002c4f01dfcfa17a065a4c9ebac582d00d6e
lcr <none@none>
2012-08-14 00:56:23 -0600

    - bump
    --HG--
    branch : pu

--- stuf/__init__.py
+++ stuf/__init__.py
@@ -3,6 +3,6 @@
 
 from stuf.core import defaultstuf, fixedstuf, frozenstuf, orderedstuf, stuf
 
-__version__ = (0, 9, 1)
+__version__ = (0, 9, 2)
 
 __all__ = 'defaultstuf fixedstuf frozenstuf orderedstuf stuf'.split()
