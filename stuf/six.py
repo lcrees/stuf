f83f5e7f8eb9746774b27678b03d9211b5c22910
lcr <none@none>
2012-08-29 17:13:43 -0600

    - changes from mother six
    --HG--
    branch : pu

--- stuf/six.py
+++ stuf/six.py
@@ -40,20 +40,23 @@ else:
     classes = (type, types.ClassType)
     texts = unicode
     native = binaries = str
-
-    # It's possible to have sizeof(long) != sizeof(Py_ssize_t).
-    class X(object):
-        def __len__(self):
-            return 1 << 31
-    try:
-        len(X())
-    except OverflowError:
-        # 32-bit
+    if sys.platform == "java":
+        # Jython always uses 32 bits.
         MAXSIZE = int((1 << 31) - 1)
     else:
-        # 64-bit
-        MAXSIZE = int((1 << 63) - 1)
-    del X
+        # It's possible to have sizeof(long) != sizeof(Py_ssize_t).
+        class X(object):
+            def __len__(self):
+                return 1 << 31
+        try:
+            len(X())
+        except OverflowError:
+            # 32-bit
+            MAXSIZE = int((1 << 31) - 1)
+        else:
+            # 64-bit
+            MAXSIZE = int((1 << 63) - 1)
+        del X
 
 
 class _LazyDescr(object):
